1043adf92752d2d91a4af1f934ec3419a1c33982
MattCromwell <mattcromwell@pop-os.localdomain>
2020-07-26 00:25:44 +0100

    fixed menu underline issue

--- static/js/scrolling.js
+++ static/js/scrolling.js
@@ -24,6 +24,7 @@ window.addEventListener('scroll', function() {
     if (window.scrollY >= 50){
         logo.setAttribute('width',smallWH[0]);
         logo.setAttribute('height',smallWH[1]);
+        logo.style.webkitFilter = 'invert(1) sepia(100%) saturate(300%) brightness(70%) hue-rotate(140deg)';
         logo.style.paddingTop = '10px';
         header.style.boxShadow = "10px 10px 9px 0px rgba(0,0,0,0.04)";
         header.style.background = '#fff';
@@ -38,11 +39,12 @@ window.addEventListener('scroll', function() {
     } else{
         logo.setAttribute('width',bigWH[0]);
         logo.setAttribute('height',bigWH[1]);
+        logo.style.webkitFilter = 'invert(1)';
         logo.style.paddingTop = '25px';
         header.style.boxShadow = "10px 10px 9px 0px rgba(0,0,0,0)";
         header.style.background = '#9BB8D2';
         activeLink.style.color = '#fff';
-        activeLink.style.borderColor = '#9BB8D2';
+        activeLink.style.borderColor = '#fff';
         links.forEach(element => {
             element.style.color = '#fff';
  
@@ -94,3 +96,4 @@ function HowHelp(){
     });
     document.getElementById('hcih').setAttribute('class','nav-active');
 }
+
